0af68af02fd8fc1e00474eed7a32ba714b8ef35c
Michael McGuire <michael@mcguire.ch>
2021-01-12 17:31:44 -0500

    add species enum and carnivore? method

--- app/models/dinosaur.rb
+++ app/models/dinosaur.rb
@@ -1,3 +1,18 @@
 class Dinosaur < ApplicationRecord
   belongs_to :cage, counter_cache: true
+
+  enum species = {
+    tyrannosaurus: 'tyrannosaurus',
+    velociraptor: 'velociraptor',
+    spinosaurus: 'spinosaurus',
+    megalosaurus: 'megalosaurus',
+    brachiosaurus: 'brachiosaurus',
+    stegosaurus: 'stegosaurus',
+    ankylosaurus: 'ankylosaurus',
+    triceratops: 'triceratops'
+  }
+  
+  def carnivore?
+    species.in?(['tyrannosaurus', 'velociraptor', 'spinosaurus', 'megalosaurus'])
+  end
 end
